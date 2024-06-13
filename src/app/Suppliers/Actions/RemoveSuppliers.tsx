6ce6ebef0c9ab4956dc4cfa9f58580fed9b90145
Emmanuel <Emmanuel-jnr@hotmail.com>
2024-06-12 22:18:28 -0300

    REFACTOR: refatorando codigo removendo fornecedores.

---
+++ src/app/Suppliers/Actions/RemoveSuppliers.tsx
@@ -0,0 +1,31 @@
+"use server"
+
+import { db } from "@/app/_lib/prisma";
+import { revalidatePath } from "next/cache";
+
+interface SupplierProps{
+    supplierID:string
+}
+
+export default async function RemoveSupplierDatabase(params:SupplierProps) {
+    try {
+        const getSupplier = await db.suppliers.findUnique({
+            where:{
+                id:params.supplierID
+            }
+        })
+
+        if(!getSupplier) throw new Error("Fornecedor nao encontrado!");
+
+        await db.suppliers.delete({
+            where:{
+                id:params.supplierID
+            }
+        })
+
+        revalidatePath("/Suppliers")
+        
+    } catch (error:any) {
+        throw new Error(error.message);
+    }
+}
