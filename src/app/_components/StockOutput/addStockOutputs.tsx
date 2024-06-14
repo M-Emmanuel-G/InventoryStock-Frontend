eb30780eafc6e65263a52737fbe1c8f0743ef1fe
Emmanuel <Emmanuel-jnr@hotmail.com>
2024-06-14 19:22:37 -0300

    REFACTOR: refatorando codigo saida de produtos

--- src/app/_components/StockOutput/addStockOutputs.tsx
+++ src/app/_components/StockOutput/addStockOutputs.tsx
@@ -15,47 +15,27 @@ import { useState } from "react";
 import { BASE_URL } from "../../_Constants/URL";
 import useRequestData from "@/app/_hooks/useRequestData";
 import { useSession } from "next-auth/react";
+import SaveOutputDatabase from "@/app/StockOut/Actions/SaveOutputs";
   
 export default function SaveOutput() {
 
     const session = useSession()
   
-    const [client, setClient ] = useState<string>("")
-    const [product, setProduct ] = useState<string>("")
+    const [client, setClient ] = useState<number>(0)
+    const [product, setProduct ] = useState<number>(0)
     const [quantity, setQuantity ] = useState<number>(0)
-
-    const [products] = useRequestData(`${BASE_URL}products/getallproducts`)
-    const [clients] = useRequestData(`${BASE_URL}clients/getallclients`)
-
-    const showClients = clients.map((client:any, key: number) =>{
-        return (
-            <option onClick={()=>{setClient(client.id)}} key={key}>{client.name}</option>
-        )
-    })
-
-    const showProducts = products.map((prod:any, key: number) =>{
-        return (
-            <option onClick={()=>{setProduct(prod.id)}} key={key}>{prod.product}</option>
-        )
-    })
 
     const saveOutput = async (ev:any)=>{
         ev.preventDefault()
 
         const body = {
-            qtd: quantity
+            qtdPurchase: quantity,
+            codProduct:product,
+            codClient: client
         }
 
-        axios
-            .post(`${BASE_URL}outputs/create/clientID/${client}/productID/${product}/userID/${session.data?.user.id}`, body)
-            .then((res)=>{
-                alert(res.data.message)
-            })
-            .catch((err)=>{
-                alert(err.response.data)
-                console.log(err.response.data)
-            
-            })
+        const response = await SaveOutputDatabase(body)
+        alert(response)
     }
 
     return (
@@ -65,27 +45,40 @@ export default function SaveOutput() {
         </DialogTrigger>
         <DialogContent>
           <DialogHeader>
-            <DialogTitle className="text-center text-2xl my-4">Novo cliente</DialogTitle>
-            <DialogDescription className="text-center my-4 text-xl">Insira os dados do novo cliente.</DialogDescription>
+            <DialogTitle className="text-center text-2xl my-4">Saída de Produto</DialogTitle>
+            <DialogDescription className="text-center my-4 text-xl"></DialogDescription>
             <DialogDescription>
                 <form onSubmit={saveOutput}>
-                    <Input
-                        value={quantity}
-                        onChange={(ev)=>{setQuantity(Number(ev.target.value))}}
-                        placeholder="Nome completo"
-                        className=" text-center text-black text-sm my-4"
-                        type="number"
-                    />
-                    <div className="w-full justify-center items-center flex flex-col">
-                        <select
-                            className="w-80 h-8 my-4 text-sm text-center bg-transparent">
-                            {showClients}
-                        </select>
-                        <select
-                            className="w-80 h-8 my-4 text-sm text-center bg-transparent">
-                            {showProducts}
-                        </select>
+                    <div className="flex flex-col ">
+                        <strong>Codigo do Cliente</strong>
+                        <Input
+                            value={client}
+                            onChange={(ev)=>{setClient(Number(ev.target.value))}}            
+                            className=" text-center text-black text-sm my-4"
+                            type="number"
+                            placeholder="Codigo do cliente"
+                        />
                     </div>
+                    <div className="flex flex-col ">
+                        <strong>Codigo do Produto</strong>
+                        <Input
+                            value={product}
+                            onChange={(ev)=>{setProduct(Number(ev.target.value))}}            
+                            className=" text-center text-black text-sm my-4"
+                            type="number"
+                            placeholder="Codigo do produto"
+                        />
+                    </div>
+                    <div className="flex flex-col ">
+                        <strong>Quantidade:</strong>
+                        <Input
+                            value={quantity}
+                            onChange={(ev)=>{setQuantity(Number(ev.target.value))}}            
+                            className=" text-center text-black text-sm my-4"
+                            type="number"
+                        />
+                    </div>
+                  
                     <div className="w-full flex justify-center">
                         <Button className="w-64">Cadastrar</Button>
                     </div>
